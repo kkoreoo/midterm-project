fc5f935c62fd197b6b5df9e9601bc8a84c3e9907
atle <alextle24@gmail.com>
2024-02-13 10:19:23 -0700

    created modal for new task, added drop menu to filter by

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+
+// Load up jQuery
+$(function() {
+
+  // Requests new task form when button clicked
+  const $newTask = $('.new-task');
+
+  $newTask.on("click", function(event) {
+    console.log(event);
+  })
+
+
+});
