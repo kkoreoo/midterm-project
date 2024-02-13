9bd20c61fa94108809bda1c60239a19d8d2ba3a9
Yasin Hussein <bulaale116@gmail.com>
2024-02-13 14:43:51 -0700

    implement user routes

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,28 @@
-const db = require('../connection');
+  const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
-};
+    const getUsers = () => {
+      const allUsers = `
+      SELECT * FROM users;
+      `;
+      return db.query(allUsers);
+    };
 
-module.exports = { getUsers };
+    const getOnlyOneUser = (userId) => {
+
+      const oneUser = `
+      SELECT * FROM users WHERE id = $1;
+      `;
+      return db.query(oneUser,[userId]);
+
+    };
+    const updateUser = (newFirstName, newLastName) => {
+      const updateQuery = `
+        UPDATE users
+        SET first_name = $1, last_name = $2
+        WHERE id = 1
+        RETURNING *
+      `;
+      return db.query(updateQuery, [newFirstName, newLastName]);
+    };
+
+module.exports = { getUsers, getOnlyOneUser,updateUser};
--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,62 @@
 
 const express = require('express');
 const router  = express.Router();
+const userinfo = require('../db/queries/users');
 
 router.get('/', (req, res) => {
-  res.render('users');
-}); 
+  userinfo.getUsers()
+  .then((result)=>{
+    const templateVariables = {
+      users:result.rows
+    };
+    res.render('users',templateVariables);
+  })
+});
+router.get('/:id', (req, res) => {
+  const userId = req.params.id
+  userinfo.getOnlyOneUser(userId)
+  .then((result)=>{
+    if (result.rows.length === 0) {
+      res.status(404).send('User not found');
+    }
+    const templateVariables = {
+      users:result.rows
+    };
+    res.render('users',templateVariables);
+  })
+});
+
+// don't forget to change this to post to make a post requet
+router.get('/:id/edit', (req, res) => {
+  const userId = req.params.id;
+  // const newFirstName = req.body.newFirstName;
+  // const newLastName = req.body.newLastName;
+
+  // Fetch the current user information
+  userinfo.getOnlyOneUser(userId)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return res.status(404).send('User not found');
+      }
+
+      // Update the user's first name and last name
+      return userinfo.updateUser('Alex','Alex2');
+    })
+
+    .then((updatedUser) => {
+      // Check if a user was updated
+      console.log('updatedUser',updatedUser);
+      if (updatedUser.rows.length === 0) {
+        return res.status(500).send('Error updating user');
+      }
+
+      const templateVariables = {
+        users: updatedUser.rows,
+        message: 'User information updated successfully',
+      };
+      // console.log(templateVariables);
+      res.render('users', templateVariables);
+    });
+});
 
 module.exports = router;
