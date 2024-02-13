96d8342751e462cdfb32c34cde0cfaa9caaf46fc
atle <alextle24@gmail.com>
2024-02-13 14:09:47 -0700

    tasks now displays the appropraite catgeory icon instead of number

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,13 +15,32 @@ $(function() {
     const taskName = taskData.task_name;
     const taskStatus = taskData.task_status;
     const category = taskData.category_id; // film=1, foods=2, books=3, products=4
+    let categoryIcon = '';
+
+    // Film Icon
+    if (category === 1) {
+      categoryIcon = '<i class="fa-solid fa-film"></i>';
+
+    // Foods Icon
+    } else if (category === 2) {
+      categoryIcon = '<i class="fa-solid fa-utensils"></i>';
+
+    // Books Icon
+    } else if (category === 3) {
+      categoryIcon = '<i class="fa-solid fa-book"></i>';
+
+    // Products Icon
+    } else if (category == 4) {
+      categoryIcon = '<i class="fa-solid fa-cart-shopping"></i>';
+    }
+
     let $task = ``;
 
     // Template for completed tasks
     if (taskStatus) {
       $task = `
       <article class="tasks-list">
-      <div class="task-icon">${category}</div>
+      <div class="task-icon">${categoryIcon}</div>
 
       <span class="task-content">
         <div class="task-complete is-completed"><i class="fa-solid fa-square-check"></i></div>
@@ -40,7 +59,7 @@ $(function() {
     } else {
       $task = `
       <article class="tasks-list">
-      <div class="task-icon">${category}</div>
+      <div class="task-icon">${categoryIcon}</div>
 
       <span class="task-content">
         <div class="task-complete"><i class="fa-regular fa-square"></i></div>
