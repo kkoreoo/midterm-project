f8d80ee9be3a94327409d3dbdb2b6506384f5c86
atle <alextle24@gmail.com>
2024-02-14 20:31:11 -0700

    function to check google books api is working correctly

--- api-calls.js
+++ api-calls.js
@@ -2,15 +2,23 @@ const axios = require('axios');
 require('dotenv').config();
 
 
+const queryString = 'Harry Potter'
+
 // Request to Google Books
 const checkGoogleBooks = (taskString) => {
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers&key=${process.env.GOOGLE_API}`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${queryString}&key=${process.env.GOOGLE_API}`)
   .then((data) => {
-    console.log('success', data.data);
+    if (data.data.totalItems !== 0) {
+      return true;
+    } else {
+      console.log('hello');
+      return null;
+    }
   })
   .catch((error) => {
     console.log('error msg:', error.response);
   })
 };
 
-checkGoogleBooks();
+
+
