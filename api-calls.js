14d4edcc07517bffe72cc7a5da75ef84980607c8
atle <alextle24@gmail.com>
2024-02-14 21:40:11 -0700

    yelp api call works

--- api-calls.js
+++ api-calls.js
@@ -17,8 +17,36 @@ const checkGoogleBooks = (taskString) => {
   })
   .catch((error) => {
     console.log('error msg:', error.response);
-  })
+  });
+};
+
+// need to figure out how to find location or we hard code it.
+const checkYelp = (taskString, city) => {
+  const options = {
+    method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search',
+    params: {
+      term: `${taskString}`,
+      location: city,
+      categories: 'food&categories=resturant',
+      sort_by: 'best_match',
+      limit: '20'
+    },
+    headers: {accept: 'application/json', Authorization: `Bearer ${process.env.YELP_API}`}
+  };
+
+  axios.request(options)
+    .then(function (response) {
+      console.log('success', response.data);
+      console.log(response.data.businesses[0].categories);
+    })
+    .catch(function (error) {
+      console.error('error msg:', error.response);
+    });
+
 };
 
 
+
+module.exports = { checkGoogleBooks, checkYelp };
 
