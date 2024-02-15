7595fc36f2c73da198ba509f09ed58d2db363a9d
Yasin Hussein <bulaale116@gmail.com>
2024-02-15 09:43:42 -0700

    Implement gategorize function except APIs

--- categorize.js
+++ categorize.js
@@ -4,6 +4,30 @@ const keywordsToWatch = ['watch', 'movie', 'film', 'show', 'tv'];
 const keywordsToEat = ['eat', 'food', 'recipe', 'restaurant', 'cafe', 'meat', 'vegetable', 'dairy', 'vitamins'];
 const keywordsToRead = ['read', 'book', 'journal', 'novel', 'newspaper', 'textbook'];
 const keywordsToBuy = ['buy', 'store', 'retail', 'purchase'];
+const dictionary = [keywordsToWatch, keywordsToEat, keywordsToRead, keywordsToBuy];
+const categories = ['Film / Series (To watch)','Restaurants, cafes, etc. (To eat)',
+'Books (To read)','Products (To buy)'];
 
 // if taskString has .includes() any of the keywords, we can automatically categorize,
 // else call apis, using promise.all,
+//the taskString value will be received dynamically from the user
+let taskString = "buy a laptop";
+//Convert to lowerCase in case upperCase text is provided
+let result = taskString.toLowerCase();
+//split the taskString and store result varaible
+const task = result.split(' ');
+const categorizeTask = function(){
+  for (let i in dictionary) {
+
+    const categoryKeywords = dictionary[i];
+
+    const matchingKeywords = categoryKeywords.filter(keyword => task.includes(keyword));
+
+    if (matchingKeywords.length > 0) {
+
+        return categories[i];
+   }
+}
+}
+
+
